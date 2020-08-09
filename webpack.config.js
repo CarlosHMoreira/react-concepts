925fe242e4b0e120947673e9851d7412fffc9f6a
Carlos Henrique da Silva Moreira <infobase.cmoreira@terceiros.mongeralaegon.com.br>
2020-08-09 00:11:54 -0300

    Configure webpack to load sass files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -12,14 +15,34 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.s(a|c)ss/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+    }),
   ],
   devServer: {
     historyApiFallback: true,
   },
-}; 
+};
