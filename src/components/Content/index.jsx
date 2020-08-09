ffb13f061a3dd602017276bd9d6df3a6d6f41e06
Carlos Henrique da Silva Moreira <infobase.cmoreira@terceiros.mongeralaegon.com.br>
2020-08-09 01:22:05 -0300

    Creates main content component

---
+++ src/components/Content/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Grid, Container, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: `${theme.spacing(2)} ${theme.spacing(1)}`,
+  },
+}));
+
+const Content = ({ children }) => {
+  const { container } = useStyles();
+  return (
+    <Container maxWidth="lg" className={container}>
+      <Grid container direction="column" justify="center" alignItems="stretch">
+        {children}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Content;
