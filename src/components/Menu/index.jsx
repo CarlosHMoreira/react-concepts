4e31c1a2ef6820fe39f624282c5ba4fdf4ed9c8b
Carlos Henrique da Silva Moreira <infobase.cmoreira@terceiros.mongeralaegon.com.br>
2020-08-09 01:55:22 -0300

    Adjusts Menu styles

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 
 const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
   fullList: {
     width: 'auto',
   },
 });
 
 const Menu = ({ onClose, isOpened, children }) => {
-  const classes = useStyles();
+  const { fullList } = useStyles();
 
   return (
     <>
       <Drawer anchor="bottom" open={isOpened} onClose={onClose}>
         <div
-          className={clsx(classes.list, classes.fullList)}
+          className={fullList}
           role="presentation"
           onClick={onClose}
           onKeyDown={onClose}
