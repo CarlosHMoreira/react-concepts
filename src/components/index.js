d94f06b4e7ce0ca9357ba16109cbbfa318cfc6cf
Carlos Henrique da Silva Moreira <infobase.cmoreira@terceiros.mongeralaegon.com.br>
2020-08-09 01:56:00 -0300

    Creates barrel

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as Menu } from './Menu';
+export { default as Content } from './Content';
+export { default as Topic } from './Topic';
+export { default as MenuListItem } from './MenuListItem';
